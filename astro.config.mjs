a1354604358d8f87e9fd9389cb5976708e489882
Mauro Longano <mauro.longano@marconirovereto.it>
2025-01-12 22:38:29 +0100

    chore(dev): add tailwind inspector in Astro toolbar

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,13 @@ import tailwind from "@astrojs/tailwind";
 
 import node from "@astrojs/node";
 
+import tailwindConfigViewer from "astro-tailwind-config-viewer";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind({
-      nesting: true,
-    }),
-  ],
+  integrations: [tailwind({
+    nesting: true,
+  }), tailwindConfigViewer()],
 
   adapter: node({
     mode: "standalone",
